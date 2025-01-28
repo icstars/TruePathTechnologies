bc2415f92a1113a5446c9a7dd3988fece51018b0
Mia Taitelieva <mtaitelieva@icstars.org>
2025-01-28 10:20:08 -0600

    set up backend folder with express env

--- frontend/src/pages/directory/Directory.jsx
+++ frontend/src/pages/directory/Directory.jsx
@@ -1,10 +1,8 @@
-import SearchBox from "../../widgets/searchbox/Searchbox"
 
 export default function Directory() {
   return (
     <div>
       <p>Directory</p>
-      <SearchBox/>
     </div>
   );
 };
