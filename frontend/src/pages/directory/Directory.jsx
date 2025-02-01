788b643719cbdb4bfce1899c719cd5a9d8e3a5eb
Travis Rosario <trosario@icstars.org>
2025-02-01 12:12:12 -0600

    removed filterbar from directory

--- frontend/src/pages/directory/Directory.jsx
+++ frontend/src/pages/directory/Directory.jsx
@@ -1,10 +1,8 @@
-import FilterBar from '../../widgets/filterBar/FilterBar'
 
 export default function Directory() {
   return (
     <div>
       <p>Directory</p>
-      <FilterBar/>
     </div>
 
   );
