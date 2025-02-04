ca9dfcbc81769681b381cd37232ec4cd9ea620a4
Travis Rosario <trosario@icstars.org>
2025-02-03 18:35:55 -0600

    mockdata added and routed

---
+++ backend/mockdata/resources.js
@@ -0,0 +1,32 @@
+[
+{
+    "websites": [
+      {
+        "name": "Mind - Five Ways to Wellbeing",
+        "url": "https://www.mind.org.uk/workplace/mental-health-at-work/five-ways-to-wellbeing/",
+        "description": "Provides five key actions to improve mental wellbeing at work."
+      },
+      {
+        "name": "SHRM - Mental Health, HR, and the Workplace",
+        "url": "https://www.shrm.org/topics-tools/news/all-things-work/mental-health--hr-and-the-workplace",
+        "description": "Covers workplace mental health issues and HR strategies for a supportive work environment."
+      },
+      {
+        "name": "SAMHSA - Find Support",
+        "url": "https://www.samhsa.gov/find-support",
+        "description": "Offers resources for individuals seeking mental health and substance use support."
+      },
+      {
+        "name": "NIMH - Health Publications",
+        "url": "https://www.nimh.nih.gov/health/publications",
+        "description": "Provides educational resources on various mental health topics from the National Institute of Mental Health."
+      },
+      {
+        "name": "Illinois DHS - Mental Health Services",
+        "url": "https://www.dhs.state.il.us/page.aspx?item=29763",
+        "description": "Offers mental health services and resources provided by the Illinois Department of Human Services."
+      }
+    ]
+  }
+]
+    
