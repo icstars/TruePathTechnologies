5b9357a9cf91d34a2ab88e63c7aa99d7db6ce9eb
Mia Taitelieva <mtaitelieva@icstars.org>
2025-02-04 22:20:59 -0600

    add component ResourceCard and update styles in Resource page

--- frontend/src/pages/resources/ResourcesPage.jsx
+++ frontend/src/pages/resources/ResourcesPage.jsx
@@ -1,9 +1,28 @@
-
+import { useEffect, useState } from "react";
+import styles from "./ResourcesPage.module.css"
+import ResourceCard from "../../widgets/resourceCard/ResourceCard";
 
 export default function ResourcesPage() {
+    const [resourcesList, setResourcesList] = useState([]);
+  
+      useEffect(() => {
+        async function getAllResources() {
+          let response = await fetch("http://localhost:5000/resources");
+          let resources = await response.json();
+          setResourcesList(resources);
+        }
+        getAllResources();
+      }, []);
+
   return (
-    <div>
-      Resources
+    <div className={styles.resourcesPage}>
+      <p className={styles.dashTitles}>Resources</p>
+      <div>
+        {
+          resourcesList.map((item, index)=> <ResourceCard key={index} {...item}/>)
+        }
+      </div>
+      
     </div>
   )
 }
